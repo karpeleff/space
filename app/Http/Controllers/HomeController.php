35be2f30ee63f43eeb7059ed7d74905fb0d07640
Alesha <karoelev1976@gmail.com>
2023-08-16 12:23:05 +1000

    папка

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,8 +39,7 @@ class HomeController extends Controller
     {
         return view('home');
     }
-
-
+    
     public  function sendMail()
     {
         $details['to'] = 'alexey.karpelev@yandex.ru';
