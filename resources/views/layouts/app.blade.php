c537df5672f79d0b95fc555c3dfad496fa7bd36a
aleksey <alekseykarpelev@gmail.com>
2023-09-19 10:58:36 +1000

    edit лого2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <img src= {{asset("assets/images/favicons/logo.png" )}} width="50" height="50">
+                <img src= {{asset("assets/images/favicons/logo.png" )}} width="150" height="150">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
